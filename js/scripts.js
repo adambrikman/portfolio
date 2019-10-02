301c658eb51543ba134d2e02c19eaaadee5e03c1
Adam <adambrikman@gmail.com>
2019-10-01 23:41:54 -0700

    added js for burger & resize of block elements. Also added GIFs

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,29 @@ burger.addEventListener("click", () => {
   }
   return list.classList.add("open");
 });
+
+// Close burger when list-item clicked
+const menu = document.querySelector(".menu");
+menu.addEventListener("click", () => {
+  return list.classList.remove("open");
+});
+
+/* Reordering About Me - First picture & paragraph */
+
+let pic = document.querySelector(".first");
+let para = document.querySelector(".second");
+
+window.addEventListener("resize", () => {
+  if (document.documentElement.clientWidth <= 900) {
+    para.parentNode.removeChild(para);
+    pic.parentNode.insertBefore(para, pic);
+  }
+});
+
+// When screen is larger than 900px, reset order back to normal
+window.addEventListener("resize", () => {
+  if (document.documentElement.clientWidth > 900) {
+    pic.parentNode.removeChild(pic);
+    para.parentNode.insertBefore(pic, para);
+  }
+});
