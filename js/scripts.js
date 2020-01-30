9d9ef955f41214f7dac01e61a334791bc6edaf4b
Adam <adambrikman@gmail.com>
2020-01-30 09:04:46 -0800

    Update syling on form, project buttons and project icons

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ menu.addEventListener("click", () => {
 let pic = document.querySelector(".first");
 let para = document.querySelector(".second");
 
-var reorderParagraphs = function() {
+var reorderParagraphs = function () {
   if (document.documentElement.clientWidth <= 900) {
     para.parentNode.removeChild(para);
     pic.parentNode.insertBefore(para, pic);
@@ -64,7 +64,49 @@ function emailSentSnackbar() {
 
   snackbar.className = "show";
 
-  setTimeout(function() {
+  setTimeout(function () {
     snackbar.className = snackbar.className.replace("show", "");
   }, 3000);
 }
+
+// Project button hover effect
+document.querySelectorAll('.btn--project').forEach(button => {
+
+  let div = document.createElement('div'),
+    letters = button.textContent.trim().split('');
+
+  function elements(letter, index, array) {
+
+    let element = document.createElement('span'),
+      part = (index >= array.length / 2) ? -1 : 1,
+      position = (index >= array.length / 2) ? array.length / 2 - index + (array.length / 2 - 1) : index,
+      move = position / (array.length / 2),
+      rotate = 1 - move;
+
+    element.innerHTML = !letter.trim() ? '&nbsp;' : letter;
+    element.style.setProperty('--move', move);
+    element.style.setProperty('--rotate', rotate);
+    element.style.setProperty('--part', part);
+
+    div.appendChild(element);
+
+  }
+
+  letters.forEach(elements);
+
+  button.innerHTML = div.outerHTML;
+
+  button.addEventListener('mouseenter', e => {
+    if (!button.classList.contains('out')) {
+      button.classList.add('in');
+    }
+  });
+
+  button.addEventListener('mouseleave', e => {
+    if (button.classList.contains('in')) {
+      button.classList.add('out');
+      setTimeout(() => button.classList.remove('in', 'out'), 950);
+    }
+  });
+
+});
